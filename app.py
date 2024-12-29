2322eb55ce79881b3f8daaef10503095cebc1e69
Jagasabarivel K <Jaga.Karunanithi@dellteam.com>
2024-12-29 07:15:36 +0530

    Add notification processing for schedule message

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ app = Flask(__name__)
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")  # Retrieve VERIFY_TOKEN
 SECRET_KEY = os.getenv("SECRET_KEY")      # Retrieve SECRET_KEY
 
+registered_phone_number_last_sent = {}
+registered_phone_number_last_received = {}
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def whatsapp_webhook():
+    recipient_id = None
+    status = None
+    error_details = None
     if request.method == 'GET':
         # Verification request from WhatsApp
         mode = request.args.get('hub.mode')
@@ -29,8 +35,24 @@ def whatsapp_webhook():
         # Handle incoming messages
         try:
             data = request.get_json()
-            print("Received data:", data)
+            notification = data
+            value = notification.get('entry')[0].get('changes')[0].get('value')
+            phone_number_id = value.get('metadata').get('phone_number_id')
+            if value.get('statuses'):
+                recipient_id = value.get('statuses')[0].get('recipient_id')
+                status = value.get('statuses')[0].get('status') 
+                if status in ['failed']:
+                    error_details = value.get('statuses')[0].get('errors')[0].get('error_data').get('details')
+                registered_phone_number_last_sent.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'status': status})
+                registered_phone_number_last_sent.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'error_details': error_details})
+            elif value.get('messages'):
+                recipient_id = value.get('messages')[0].get('from')
+                if value.get('messages')[0].get('type') == 'text':
+                    message = value.get('messages')[0].get('text').get('body')
+                    timestamp = value.get('messages')[0].get('timestamp')
+                    registered_phone_number_last_received.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'received_time': timestamp, 'message': message})
 
+            print("Received data:", notification)
             # Extract necessary information
             if data and 'messages' in data:
                 messages = data['messages']
@@ -45,6 +67,12 @@ def whatsapp_webhook():
         except Exception as e:
             print("Error:", e)
             return jsonify({"status": "error", "message": str(e)}), 400
+
+@app.route('/schedule/notifications/<phone_number_id>', methods=['GET'])
+def get_last_message_status_details_from_recipient_number(phone_number_id):
+    print("Received phone number id:", registered_phone_number_last_sent)
+    return jsonify(registered_phone_number_last_sent.get(phone_number_id, {}))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
