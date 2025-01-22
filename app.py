72107d05facecb0d9ada45c9246f77131ab37490
K <jaga.karunanithi@dellteam.com>
2025-01-22 13:02:32 +0530

    Add message broker to whatsapp webhook

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import os
+import json
+
 from flask import Flask, request, jsonify
+from confluent_kafka import Producer, KafkaError
 
 app = Flask(__name__)
+
+# Kafka Configuration
+TOPIC = 'whatsapp'
+
+def read_config():
+  # reads the client configuration from client.properties
+  # and returns it as a key-value map
+  config = {}
+  with open("kafka.config") as fh:
+    for line in fh:
+      line = line.strip()
+      if len(line) != 0 and line[0] != "#":
+        parameter, value = line.strip().split('=', 1)
+        config[parameter] = value.strip()
+  return config
+
+producer_config = read_config()
+producer_config["sasl.username"] = os.getenv("kf_username")
+producer_config["sasl.password"] = os.getenv("kf_password")
 
 # Replace 'your_verify_token' with your actual verify token
 # Load environment variables
@@ -10,6 +32,44 @@ SECRET_KEY = os.getenv("SECRET_KEY")      # Retrieve SECRET_KEY
 
 registered_phone_number_last_sent = {}
 registered_phone_number_last_received = {}
+
+
+producer = Producer(producer_config)
+def delivery_report(err, msg):
+    """
+    Callback for delivery reports.
+    Called once for each message produced to indicate delivery result.
+    """
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+def publish_message(topic, msg):
+    try:
+        # Produce a message asynchronously
+        producer.produce(
+            topic,
+            value=json.dumps(msg),
+            callback=delivery_report  # Attach the delivery callback
+        )
+        # Ensure messages are flushed to Kafka
+        producer.flush()
+    except KafkaError as e:
+        print(f"Failed to produce message: {e}")
+
+def send_msg(phone_number_id, from_user, msg, msg_type):
+    try:
+        data = request.json
+        if msg_type == "text":
+            message = {
+                "recipient_id": from_user,
+                "message_body": msg,
+                "phone_number_id": phone_number_id
+            }
+        publish_message(TOPIC, message)
+    except Exception as e:
+        raise Exception("Failed to publish message to broker") from e
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def whatsapp_webhook():
@@ -46,23 +106,24 @@ def whatsapp_webhook():
                 registered_phone_number_last_sent.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'status': status})
                 registered_phone_number_last_sent.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'error_details': error_details})
             elif value.get('messages'):
-                recipient_id = value.get('messages')[0].get('from')
-                if value.get('messages')[0].get('type') == 'text':
-                    message = value.get('messages')[0].get('text').get('body')
-                    timestamp = value.get('messages')[0].get('timestamp')
-                    registered_phone_number_last_received.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'received_time': timestamp, 'message': message})
+                messages = value.get('messages')
+                for message in messages:
+                    recipient_id = message.get('from')
+                    if message.get('type') == 'text':
+                        message = message.get('text').get('body')
+                        timestamp = message.get('timestamp')
+                        registered_phone_number_last_received.setdefault(phone_number_id, {}).setdefault(recipient_id, {}).update({'received_time': timestamp, 'message': message})
 
             print("Received data:", notification)
             # Extract necessary information
-            if data and 'messages' in data:
-                messages = data['messages']
+            if notification and 'messages' in value:
+                messages = value.get('messages')
                 for message in messages:
                     sender = message.get('from')  # Sender's phone number
                     message_text = message.get('text', {}).get('body')  # Message text
                     print(f"Message from {sender}: {message_text}")
-
+                    send_msg(phone_number_id=phone_number_id, from_user=sender, msg=message_text, msg_type="text")
                     # Process the message (e.g., respond, store in database, etc.)
-            
             return jsonify({"status": "success"}), 200
         except Exception as e:
             print("Error:", e)
@@ -72,6 +133,9 @@ def whatsapp_webhook():
 def get_last_message_status_details_from_recipient_number(phone_number_id):
     print("Received phone number id:", registered_phone_number_last_sent)
     return jsonify(registered_phone_number_last_sent.get(phone_number_id, {}))
+
+
+
 
 
 if __name__ == '__main__':
